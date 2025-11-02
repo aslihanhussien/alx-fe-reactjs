149e6c4c5ad52d34f544950f3b235c4d2b779014
aslihanhussien <aslihan.hussien@gmail.com>
2025-11-02 22:19:02 +0200

    feat: Completed Task 1 - created and modified WelcomeMessage component

---
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -0,0 +1,13 @@
+function WelcomeMessage() {
+    return (
+        <div>
+            {/* Change the <h1> tag text */}
+            <h1>Hello everyone, I am learning React at ALX!</h1> 
+            <p>This is a simple JSX component.</p>
+            {/* Add a new paragraph tag */}
+            <p>I am learning about JSX!</p>
+        </div>
+    );
+}
+
+export default WelcomeMessage;
